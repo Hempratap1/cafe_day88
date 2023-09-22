645126bbdc362f133d3e003bbc76ee3cca49808e
Hempratap Singh Solanki <hempratap2003@gmail.com>
2023-09-22 18:03:35 +0530

    ok

---
+++ main.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField, IntegerField
+from wtforms.validators import DataRequired, URL
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
+
+# SQLite database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafe.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable SQLAlchemy event tracking
+db = SQLAlchemy(app)
+
+
+# Define the Cafe model
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    map_url = db.Column(db.String(255), nullable=False)
+    img_url = db.Column(db.String(255), nullable=False)
+    location = db.Column(db.String(20), nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    seats = db.Column(db.Integer, nullable=False)
+    coffee_price = db.Column(db.String(20), nullable=False)
+
+
+# Define the CafeForm
+class CafeForm(FlaskForm):
+    name = StringField('Cafe name', validators=[DataRequired()])
+    map_url = StringField('Cafe Location on Google Maps (URL)', validators=[DataRequired(), URL()])
+    img_url = StringField('Cafe Image URL', validators=[DataRequired(), URL()])
+    location = StringField('Location e.g. City', validators=[DataRequired()])
+    has_sockets = BooleanField('Has Sockets', default=False, id='has_sockets')
+    has_toilet = BooleanField('Has Toilet', default=False, id='has_toilet')
+    has_wifi = BooleanField('Has WiFi', default=False, id='has_wifi')
+    can_take_calls = BooleanField('Can Take Calls', default=False, id='can_take_calls')
+    seats = IntegerField('Number of Seats', validators=[DataRequired()])
+    coffee_price = StringField('Coffee Price', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/add', methods=["GET", "POST"])
+def add_cafe():
+    form = CafeForm()
+    if form.validate_on_submit():
+        new_cafe = Cafe(
+            name=form.name.data,
+            map_url=form.map_url.data,
+            img_url=form.img_url.data,
+            location=form.location.data,
+            has_sockets=form.has_sockets.data,
+            has_toilet=form.has_toilet.data,
+            has_wifi=form.has_wifi.data,
+            can_take_calls=form.can_take_calls.data,
+            seats=form.seats.data,
+            coffee_price=form.coffee_price.data
+        )
+        db.session.add(new_cafe)
+        db.session.commit()
+        return redirect(url_for('home'))  # Redirect to the home page after adding a cafe
+    return render_template('add.html', form=form)
+
+
+@app.route('/')
+def home():
+    cafes = Cafe.query.all()
+    return render_template('index.html', cafes=cafes)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
